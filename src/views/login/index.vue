9b483b8b554aa8ef973795059b1d13cf5e786571
unknown <you@example.com>
2020-02-10 22:18:54 +0800

    登陆界面无功能

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,12 +1,83 @@
 <template>
-  <div class="component">Login</div>
+  <!-- 全屏容器 -->
+  <div class="container-login">
+    <video
+      class="cover_video"
+      src="https://sky.res.netease.com/2019/0515/4.mp4"
+      id="LoopVideo"
+      style="object-fit:cover"
+      preload
+      muted
+      loop
+    ></video>
+    <el-card class="my-card">
+      <img src="../../assets/logo_index.png" alt />
+      <!-- el-form 作用：表单容器 -->
+      <el-form :model="loginForm">
+        <el-form-item>
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            v-model="loginForm.code"
+            placeholder="请输入验证码"
+            style="width:240px;margin-right:8px"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <!-- :value="true" 选中复选框不能进行修改  必须同意 -->
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
   //使用组件的名字
-  name: "app-login"
+  data() {
+    return {
+      loginForm: {
+        mobile: "",
+        code: ""
+      }
+    };
+  }
 };
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.container-login {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  position: absolute;
+  top: 0;
+  left: 0;
+  .bgc {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  .my-card {
+    width: 400px;
+    height: 350px;
+    transform: translate(-50%, -60%);
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    img {
+      width: 200px;
+      display: block;
+      margin: 0 auto 20px;
+    }
+  }
+}
+</style>
