0491fcaa529f412c6691d7da512c4586a0cc4c7b
unknown <you@example.com>
2020-02-10 11:24:33 +0800

    项目框架

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
+import auth from '@/utils/auth'
+import Article from '@/views/article'
 Vue.use(VueRouter)
 const router = new VueRouter({
-    routers: []
+    routes: [
+        { path: '/article', component: Article }
+    ]
 })
+export default router
