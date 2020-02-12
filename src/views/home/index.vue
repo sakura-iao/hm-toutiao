513090115b6ca20ae766c78777d2731702c2acdf
unknown <you@example.com>
2020-02-12 17:25:04 +0800

    首页一级路由布局完成无功能

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,8 +1,60 @@
 <template>
   <el-container class="container-home">
-    <el-aside class="my-aside" width="200px">Aside</el-aside>
+    <el-aside class="my-aside" width="200px">
+      <div class="logo"></div>
+      <el-menu  default-active="1"
+        background-color="rgba(248, 242, 242, 0.856)"
+        text-color="#A1A1A1"
+        active-text-color="#ffd04b"
+        style="border-right:none">
+       <el-menu-item index="1">
+          <i class="el-icon-s-home"></i>
+          <span slot="title">首页</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-document"></i>
+          <span slot="title">內容管理</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-picture"></i>
+          <span slot="title">素材管理</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-s-promotion"></i>
+          <span slot="title">发布文章</span>
+        </el-menu-item>
+        <el-menu-item index="5">
+          <i class="el-icon-chat-dot-round"></i>
+          <span slot="title">评论管理</span>
+        </el-menu-item>
+        <el-menu-item index="6">
+          <i class="el-icon-present"></i>
+          <span slot="title">粉丝管理</span>
+        </el-menu-item>
+        <el-menu-item index="7">
+          <i class="el-icon-setting"></i>
+          <span slot="title">个人设置</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
     <el-container>
-      <el-header class="my-header">Header</el-header>
+      <el-header class="my-header">
+        <span class="icon el-icon-s-fold"></span>
+        <span class="text">江苏传智播客科技教育有限公司</span>
+        <el-dropdown class="my-dropdown">
+          <span class="el-dropdown-link">
+            <!-- 用户头像 -->
+            <img class="head" src="../../assets/avatar.jpg" alt />
+            <!-- 用户名称 -->
+            <strong class="name">周杰伦</strong>
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>个人设置</el-dropdown-item>
+            <el-dropdown-item>退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
       <el-main>Main</el-main>
     </el-container>
   </el-container>
@@ -16,17 +68,42 @@ export default {
 
 <style scoped lang='less'>
 .container-home {
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    left: 0;
-    top: 0;
-    .my-aside {
-        background-color: rgba(41, 185, 221, 0.555);
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  .my-aside {
+    background-color: rgba(248, 242, 242, 0.856);
+    .logo {
+      width: 100%;
+      height: 60px;
+      background: #fff url(../../assets/IMG_1332.png) no-repeat center / 140px auto;
     }
-    .my-header {
-        border-bottom: 1px solid rgba(41, 185, 221, 0.356);
+  }
+  .my-header {
+    border-bottom: 1px solid rgba(221, 200, 209, 0.616);
+    line-height: 60px;
+    .icon {
+      font-size: 24px;
+      vertical-align: middle;
     }
-
+    .text {
+      vertical-align: middle;
+      padding-left: 10px;
+    }
+    .my-dropdown {
+      float: right;
+      .head {
+        width: 30px;
+        height: 30px;
+        vertical-align: middle;
+      }
+      .name {
+        padding-left: 5px;
+        vertical-align: middle;
+      }
+    }
+  }
 }
 </style>
